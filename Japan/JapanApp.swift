4d594c89d6b40b03b1622067dab19f8b7ff0597d
Forrest Anderson <forrest@rouviere.com>
2020-12-14 09:49:01 -0700

    Initial Commit

---
+++ Japan/JapanApp.swift
@@ -0,0 +1,17 @@
+//
+//  JapanApp.swift
+//  Japan
+//
+//  Created by Forrest Anderson on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct JapanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
